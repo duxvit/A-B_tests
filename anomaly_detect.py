2305a35918094cf6f01aa2bf7296d68e4c3cbc07
Marsel Scherbinin <duxvita@gmail.com>
2022-01-09 20:16:38 +0000

    Added from CH import Getch Update anomaly_detect.py

--- anomaly_detect.py
+++ anomaly_detect.py
@@ -7,17 +7,7 @@ from datetime import date
 import io
 import sys
 import os
-
-class Getch:
-    def __init__(self, query, db='simulator'):
-        self.connection = {
-            'host': 'https://clickhouse.lab.karpov.courses',
-            'password': 'dpo_python_2020',
-            'user': 'student',
-            'database': db,
-        }
-        self.query = query
-        self.getchdf
+from CH import Getch
 
     @property
     def getchdf(self):
